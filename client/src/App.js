ccc9b1c071624d2028f69ff0956f88505a1534bc
Jhemarr Anderson <jhemarr@protonmail.com>
2023-03-29 11:54:48 -0400

    latest

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React from 'react';
 import Sidebar from './components/sidebar';
+import TaskBar from './components/taskbar'; // Import the TaskBar component
 import styles from './App.module.css';
 
 const App = () => {
@@ -8,6 +9,11 @@ const App = () => {
 		<div className={styles.container}>
 			<div className={styles.sidebarContainer}>
 				<Sidebar />
+			</div>
+			<div className={styles.taskContainer}>
+				<TaskBar /> {/* Use the TaskBar component */}
+				{/* Add your task content here */}
+				<h2>Tasks</h2>
 			</div>
 			<div className={styles.mainContainer}>
 				<div className={styles.header}>IFYKYK</div>
---
+++ client/src/components/taskbar.js
@@ -0,0 +1,14 @@
+// components/TaskBar.js
+import React from 'react';
+import styles from './Taskbar.module.css';
+
+const TaskBar = () => {
+	return (
+		<div className={styles.taskBar}>
+			{/* Add your TaskBar content here */}
+			<h3>TaskBar Content</h3>
+		</div>
+	);
+};
+
+export default TaskBar;
